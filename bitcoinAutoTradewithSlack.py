0cada2fd62b6e67fa87efabea9cc9c7ed5afb975
c17an <kimchanmin1@gmail.com>
2021-04-26 23:51:18 +0900

    스탑로스 조건 추가

--- bitcoinAutoTradewithSlack.py
+++ bitcoinAutoTradewithSlack.py
@@ -116,7 +116,8 @@ print("autotrade start")
 # 시작 메세지 슬랙 전송
 post_message(myToken, "#alarm", "매매 시작")
 # 구매시 평단
-avg_buy = 62837969
+# 무조건 바꿔줘야함
+avg_buy = 63296336
 get_ror()
 
 
@@ -139,16 +140,16 @@ while True:
             ma15 = get_ma15("KRW-BTC")
             current_price = get_current_price("KRW-BTC")
             # 거래기준 : 5일 이평선
-            #print(target_price, current_price, ma5)
-            print("현재가 : " + str(current_price) + " / 매수 목표 : " + str(target_price) +
-                  " / 매도 목표 : " + str(avg_buy * 1.004))
+            # print(target_price, current_price, ma5)
+            # print("현재가 : " + str(current_price) + " / 매수 목표 : " + str(target_price) +
+            #       " / 매도 목표 : " + str(avg_buy * 1.006))
             if target_price < current_price and ma5 < current_price:
                 print("매수 포지션: ", current_price, avg_buy, btc)
                 if krw > 5000:
                     avg_buy = current_price
                     buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
                     post_message(myToken, "#alarm",
-                                 "매수 목표가 " + str(target_price) + "달성 / " + "매수 평균단가 : " + str(avg_buy))
+                                 "매수 목표가 " + str(target_price) + " 달성 / " + "매수 평균단가 : " + str(avg_buy))
 
             if btc > 0.00008:
                 if current_price >= avg_buy * 1.006:
@@ -156,9 +157,17 @@ while True:
                     sell_result = upbit.sell_market_order(
                         "KRW-BTC", btc*0.9995)
                     post_message(myToken, "#alarm",
-                                 "매수 목표가 " + str(avg_buy * 1.006) + "달성 /" + "매도 평균단가 : " + str(avg_buy * 1.006))
+                                 "매도 목표가 " + str(avg_buy * 1.006) + " 달성 /" + "매도 평균단가 : " + str(avg_buy * 1.006))
                     avg_buy = 0
                     time.sleep(180)
+                # 스탑로스 기준 : 20% 손실
+                elif current_price <= avg_buy * 0.8:
+                                        sell_result = upbit.sell_market_order(
+                        "KRW-BTC", btc*0.9995)
+                    post_message(myToken, "#alarm", "스탑로스 발동, 매도 평균단가 : " + str(avg_buy * 0.8))
+                    avg_buy = 0
+                    time.sleep(300)
+
         else:
             if btc > 0.00008:
                 sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
